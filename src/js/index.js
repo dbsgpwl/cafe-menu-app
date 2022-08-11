84e13a2832f4f8bb1a8ca5fa04ce1eb8769061e1
yunhyeji <dbsgpwl97@gmail.com>
2022-08-11 11:30:00 +0900

    #3 - 메뉴 삭제하기 완성 메뉴 카운트의 총 메뉴 갯수도 삭제 되어야 하므로,
    메뉴 추가 때 사용하였던, menuCount 기능을 remove() 매서드 실행 후, 실행시켜줌.
    
    코드가 중복되므로, UpdatemenuCount 변수를 생성하여 각 코드 자리에 UpdatemenuCount() 를 넣어 실행시켜줌.

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,22 @@
 //step1 요구사항 구현을 위한 전략
 // TODO 메뉴 추가
-// - [ ] 메뉴의 이름을 입력 받고 엔터키 입력으로 추가한다.
-// - [ ] 메뉴의 이름을 입력 받고 확인 버튼을 클릭하면 메뉴를 추가한다.
-// - [ ] 추가되는 메뉴의 마크업은 `<ul id="espreeso-menu-list" class="mt-3 pl-0"></ul>` 안에 삽입해야 한다.
-// - [ ] 총 메뉴 갯수를 count 하여 상단에 보여준다.
-// - [ ] 메뉴가 추가되고 나면, input 은 빈 값으로 초기화한다.
-// - [ ] 사용자 입력 값이 빈 값이라면 추가되지 않는다.
+// - [x] 메뉴의 이름을 입력 받고 엔터키 입력으로 추가한다.
+// - [x] 메뉴의 이름을 입력 받고 확인 버튼을 클릭하면 메뉴를 추가한다.
+// - [x] 추가되는 메뉴의 마크업은 `<ul id="espreeso-menu-list" class="mt-3 pl-0"></ul>` 안에 삽입해야 한다.
+// - [x] 총 메뉴 갯수를 count 하여 상단에 보여준다.
+// - [x] 메뉴가 추가되고 나면, input 은 빈 값으로 초기화한다.
+// - [x] 사용자 입력 값이 빈 값이라면 추가되지 않는다.
+
+// TODO 메뉴 수정
+// - [x] 메뉴의 수정 버튼 클릭 이벤트를 받고, 메뉴 수정하는 모달창(prompt)이 뜬다.
+// - [x] 모달창에서 신규메뉴명을 입력 받고, 확인버튼을 누르면 메뉴가 수정된다.
+
+
+// TODO 메뉴 삭제
+// - [x] 메뉴 삭제 버튼 클릭 이벤트를 받고, 메뉴 삭제 컨펌(confirm) 모달창이 뜬다.
+// - [x] 확인 버튼을 클릭하면 메뉴가 삭제된다.
+// - [x] 총 메뉴 갯수를 count 하여 상단에 보여준다.
+
 
 //$ 를 이용하여 querySelector에 들어오는 id값을 받아서  "=> document.querySelector(selector)" 값을 리턴
 const $ = (selector) => document.querySelector(selector);
@@ -15,9 +26,13 @@ $("#espresso-menu-submit-button").addEventListener("click", () => {
 });
 
 function App(){
-    // - [ ] 메뉴의 수정 버튼 클릭 이벤트를 받고, 메뉴 수정하는 모달창(prompt)이 뜬다.
-    // - [ ] 모달창에서 신규메뉴명을 입력 받고, 확인버튼을 누르면 메뉴가 수정된다.
+    
+    const UpdatemenuCount = () => {
+        // 메뉴 카운트   
+        const menuCount = $("#espresso-menu-list").querySelectorAll("li").length; // const 변수(menuCount) = li 개수를 카운팅하기 // querySelectorAll : li 태그에 있는 모든 것 가져오기
+        $(".menu-count").innerText = `총 ${menuCount} 개`; //innerText : 문자값 바꾸기
 
+    }
     $("#espresso-menu-list").addEventListener("click", (e) => {
         if(e.target.classList.contains("menu-edit-button")){
             const $menuName = e.target.closest("li").querySelector(".menu-name");//return li>span에 있는 menu-name 가져오기  //closest("li") : li 가져오는 기능
@@ -31,6 +46,7 @@ function App(){
         if(e.target.classList.contains("menu-remove-button")){
             if(confirm("정말 삭제하시겠습니까?")){//confirm : 확인버튼 누르면 true를 return, 취소버튼 부르면 false를 return
               e.target.closest("li").remove();
+              UpdatemenuCount(); //메뉴 카운트 update
             }
              
         }
@@ -80,10 +96,8 @@ function App(){
             );
             
             
-            // 메뉴 카운트   
-            const menuCount = $("#espresso-menu-list").querySelectorAll("li").length; // const 변수(menuCount) = li 개수를 카운팅하기 // querySelectorAll : li 태그에 있는 모든 것 가져오기
-            $(".menu-count").innerText = `총 ${menuCount} 개`; //innerText : 문자값 바꾸기
-            
+            UpdatemenuCount();
+
             //input 박스 빈값으로 초기화 하기 -> 스크립트가 순서대로 읽기 때문!
             $("#espresso-menu-name").value = '';
             
